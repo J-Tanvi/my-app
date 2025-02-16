d779f222b8bd3f2c2aba3e961dd76e367288978f
Carolyn <carolyn.po.lee68@gmail.com>
2025-02-16 01:33:12 -0500

    ai chat bot

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import React, {useState } from 'react';
+import ChatBotIcon from "./components/ChatBotIcon"
+import ChatForm from "./components/ChatForm";
+import ChatMessage from "./components/ChatMessage";
+
+const App = () => {
+  const [chatHistory, setChatHistory] = useState([]);
+
+  const generateBotResponse = async (history) => {
+
+    const updateHistory = (text) => {
+      setChatHistory(prev => [...prev.filter(msg => msg.text !== "..."), {role: "model", text}]);
+    }
+    history = history.map(({role, text}) => ({role, parts: [{text}]}))
+    const requestOptions = {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({contents: history})
+    }
+
+    try {
+
+      const response = await fetch(import.meta.env.VITE_API_URL, requestOptions);
+      const data = await response.json();
+      if(!response.ok) throw new Error(data.error.message || "Something went wrong!");
+
+      console.log("API Response:", data);
+      const apiResponseText = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, "$1").trim();
+      updateHistory(apiResponseText);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="container">
+      <div className="chatbot-popup">
+        {/* header */}
+        <div className="chat-header">
+          <div className="header-info">
+            <ChatBotIcon />
+            <h2 className="logo-text">Chatbot</h2>
+          </div>
+          <button className="material-symbols-rounded">keyboard_arrow_down</button>
+        </div>
+
+      {/* body */}
+        <div className="chat-body">
+          <div className="message bot-message">
+            <ChatBotIcon />
+            <p className="message-text">
+              Hi! I'm Chatbot. <br />How can I help you today?
+            </p>
+          </div>
+
+        {/* Chat History */}
+          {chatHistory.map((chat, index) => (
+              <ChatMessage key={index} chat={chat}/>
+          ))}
+          
+        </div>
+        {/* Footer */}
+        <div className="chat-footer">
+          <ChatForm chatHistory={chatHistory} setChatHistory={setChatHistory} generateBotResponse={generateBotResponse}/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App
+
